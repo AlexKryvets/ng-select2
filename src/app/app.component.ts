d5c36f99cd5671483d58e69deaa1f961ed9d3ebe
Yurii Nikytenko <yurii.v.nikitenko@gmail.com>
2021-11-18 14:57:10 +0200

    dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,6 @@ import {FormControl} from '@angular/forms';
 export class AppComponent {
     title = 'ng-select2-app';
 
-    regions = new FormControl([1, 3]);
-    regions2 = new FormControl([{id: 1, name: 'First'}, {id: 3, name: 'Third'}]);
-
     data = [
         {id: 1, text: 'First', data: {id: 1}},
         {id: 2, text: 'Second', data: {id: 2}},
@@ -42,6 +39,24 @@ export class AppComponent {
         ngModel: {...this.data[2]}
     };
 
+    select2SingleFormControlSync = {
+      options: {...this.defaultSelect2Options},
+      valueId: 2,
+      value: {...this.data[1]}
+    };
+
+    select2SingleFormControlAsync = {
+      options: {...this.defaultSelect2Options},
+      valueId: 1,
+      value: {...this.data[0]}
+    };
+
+    select2SingleFormControlObservable = {
+      options: {...this.defaultSelect2Options, ...{createObservable: this.createData$.bind(this)}},
+      valueId: 3,
+      value: {...this.data[2]}
+    };
+
     select2MultipleSync = {
         options: {...this.defaultSelect2Options, ...{multiple: true}},
         model: [1, 2],
@@ -60,6 +75,24 @@ export class AppComponent {
         ngModel: [{...this.data[0]}, {...this.data[2]}]
     };
 
+    select2MultipleFormControlSync = {
+      options: {...this.defaultSelect2Options, ...{multiple: true}},
+      valueIds: [1, 2],
+      value: [{...this.data[0]}, {...this.data[1]}]
+    };
+
+    select2MultipleFormControlAsync = {
+      options: {...this.defaultSelect2Options, ...{multiple: true}},
+      valueIds: [2, 3],
+      value: [{...this.data[1]}, {...this.data[2]}]
+    };
+
+    select2MultipleFormControlObservable = {
+      options: {...this.defaultSelect2Options, ...{multiple: true}, ...{createObservable: this.createData$.bind(this)}},
+      valueIds: [1, 3],
+      value: [{...this.data[0]}, {...this.data[2]}]
+    };
+
     createData$(): Observable<any> {
         return of(this.data).pipe(delay(1000));
     }
@@ -75,4 +108,18 @@ export class AppComponent {
     ngCompareWith(value1, value2) {
         return value1 && value2 && value1.id === value2.id;
     }
+
+    regions13 = new FormControl(this.select2SingleFormControlSync.valueId);
+    regions14 = new FormControl(this.select2SingleFormControlSync.value);
+    regions15 = new FormControl(this.select2SingleFormControlAsync.valueId);
+    regions16 = new FormControl(this.select2SingleFormControlAsync.value);
+    regions17 = new FormControl(this.select2SingleFormControlObservable.valueId);
+    regions18 = new FormControl(this.select2SingleFormControlObservable.value);
+    regions19 = new FormControl(this.select2MultipleFormControlSync.valueIds);
+    regions20 = new FormControl(this.select2MultipleFormControlSync.value);
+    regions21 = new FormControl(this.select2MultipleFormControlAsync.valueIds);
+    regions22 = new FormControl(this.select2MultipleFormControlAsync.value);
+    regions23 = new FormControl(this.select2MultipleFormControlObservable.valueIds);
+    regions24 = new FormControl(this.select2MultipleFormControlObservable.value);
+
 }
