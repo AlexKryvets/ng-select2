533fe11a1eddf20a037a0189a4654245ea33ca52
alex.kryvets <alex.kryvets@gmail.com>
2022-05-30 18:50:09 +0300

    fix

--- projects/ng-select2/src/public_api.ts
+++ projects/ng-select2/src/public_api.ts
@@ -3,4 +3,5 @@
  */
 export {Select2Options} from './interfaces';
 export {Select2Component} from './select2.component';
+export {Select2OptionValueDirective} from './select2-option-value.directive';
 export {Select2Module} from './module';
