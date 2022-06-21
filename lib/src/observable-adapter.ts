c8b6b7dc5dd45f1201eabc07c9afac068d0f5a8a
alex.kryvets <alex.kryvets@gmail.com>
2022-06-21 22:24:55 +0300

    dev

--- lib/src/observable-adapter.ts
+++ lib/src/observable-adapter.ts
@@ -5,9 +5,17 @@ import {buildValueString} from './utils';
 import {Select2OptionValueDirective} from './select2-option-value.directive';
 
 export type CreateObservableFunction = (params: { term: string }) => Observable<any>;
+export interface DataAdapterOptions {
+  getOptionValue: (item: any) => any;
+}
 
 export class ObservableAdapter extends ArrayAdapter {
     private createObservable: CreateObservableFunction;
+    private dataAdapterOptions: DataAdapterOptions = {
+      getOptionValue: (item: any) => {
+        return item;
+      }
+    };
     private observableSubscription: Subscription;
     private select2Component: Select2Component;
 
@@ -15,6 +23,7 @@ export class ObservableAdapter extends ArrayAdapter {
         super($element, options);
         this.select2Component = options.get('select2Component');
         this.createObservable = options.get('createObservable') as CreateObservableFunction;
+        this.dataAdapterOptions = options.get('dataAdapterOptions') as DataAdapterOptions;
     }
 
     query(params, callback) {
@@ -23,37 +32,46 @@ export class ObservableAdapter extends ArrayAdapter {
         }
 
         this.observableSubscription = this.createObservable(params).subscribe((data) => {
-            callback({results: data});
+            callback({
+                results: data.map((item) => {
+                    const optionValue = this.dataAdapterOptions.getOptionValue(item);
+                    const id = this.select2Component.getOptionId(optionValue);
+                    item.id = buildValueString(id, optionValue);
+                    return item;
+                })
+            });
         });
     }
 
     select(data: any) {
+        const optionValue = this.dataAdapterOptions.getOptionValue(data);
         let $option = this.$element.find('option').filter((i, elm: any) => {
-            const id = this.select2Component.getOptionId(data.id);
-            return elm.value === buildValueString(id, data.id);
+            const id = this.select2Component.getOptionId(optionValue);
+            return elm.value === buildValueString(id, optionValue);
         });
 
         if ($option.length === 0) {
-            const id = this.select2Component.registerOption({value: data} as Select2OptionValueDirective);
+            const id = this.select2Component.registerOption({
+                value: this.dataAdapterOptions.getOptionValue(data)
+            } as Select2OptionValueDirective);
             $option = this.option(data);
-            $option[0].value = buildValueString(id, data.id);
+            $option[0].value = buildValueString(id, optionValue);
             this.addOptions($option);
         }
 
         let elementValue = this.$element.val();
-        if (Array.isArray(elementValue) && elementValue.indexOf($option.val()) === -1) {
-            elementValue.push($option.val());
+        if (Array.isArray(elementValue)) {
+            const index = elementValue.indexOf($option.val());
+            if (index === -1) {
+                elementValue.push($option.val());
+            } else {
+                elementValue.splice(index, 1);
+            }
         } else {
             elementValue = $option.val();
         }
 
-        data.selected = true;
-        const elementData = this.$element.select2('data');
-        if (Array.isArray(elementData) && elementData.indexOf(data) === -1) {
-            elementData.push(data);
-        }
-
         this.$element.val(elementValue);
-        this.$element.trigger('change', elementData);
+        this.$element.trigger('change', elementValue);
     }
 }
